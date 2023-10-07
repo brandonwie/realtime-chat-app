91f847e7bbb60787ddc0312d80f5e97710d1327b
Brandon Wie <brandonwie.cs@gmail.com>
2023-10-07 15:37:55 +0900

    feat(layout.tsx): add sidebar, icons

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,33 @@
+import { Icons } from '@/components/Icons';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import { FC, ReactNode } from 'react';
+
+interface layoutProps {
+  children: ReactNode;
+}
+
+const Layout: FC<layoutProps> = async ({ children }) => {
+  const session = await getServerSession(authOptions);
+  if (!session) notFound();
+
+  return (
+    <div className='w-full flex h-screen'>
+      {/* sidebar */}
+      <div className='flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
+        <Link href='/dashboard' className='flex h-16 shrink-0 items-center'>
+          <Icons.Logo className='h-8 w-auto text-indigo-600' />
+        </Link>
+        <div className='text-xs font-semibold leading-6 text-gray-400'>
+          Your chats
+        </div>
+      </div>
+
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
